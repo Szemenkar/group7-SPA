5445f1edd4429428d9581914dab50e3347c6fd0e
Clone888 <dwclone@gmail.com>
2024-08-20 15:00:34 +0200

    Rest api
    Got one route to work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Main from './main'; // Import Main component
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Privacy from "./components/Privacy";
 
 function App() {
   return (
     <div className="container">
       <h1>Group 7 - SPA</h1>
-      <Main />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
