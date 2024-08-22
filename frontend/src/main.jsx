7f9b75fc46b94d956ad6cc10238ef4ce0d9f7932
Clone888 <dwclone@gmail.com>
2024-08-22 12:56:03 +0200

    fixed a wrong import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import ArticleList from './ArticleList';
+import ArticleList from './components/ArticleList.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
